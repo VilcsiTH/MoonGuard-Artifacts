541255569e72db34622bd4840fbd9cc538c54db4
VilcsiTH <vilcsith@gmail.com>
2022-10-28 00:02:20 +0200

    heavy development

---
+++ Source/MoonguardTraits/MoonguardTraits.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+using RimWorld;
+
+namespace MoonguardTraits
+{
+    public class CompMoonguardWeapon : CompBiocodable
+    {
+        public List<WeaponTraitDef> TraitsListForReading
+        {
+            get
+            {
+                return this.traits;
+            }
+        }
+        private List<WeaponTraitDef> traits = new List<WeaponTraitDef>();
+    }
+    public class CompProperties_MoonguardWeapon : CompProperties_Biocodable
+    {
+        public CompProperties_MoonguardWeapon()
+        {
+            this.compClass = typeof(CompBladelinkWeapon);
+        }
+    }
+
+}
